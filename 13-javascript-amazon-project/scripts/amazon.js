ece79bd88929ef12b2d71a6c452d794be3ed8eb1
SevenWoos <swenwu96@gmail.com>
2026-06-22 16:24:16 -0700

    13k. Add class to the message element using .classList.add().

--- 13-javascript-amazon-project/scripts/amazon.js
+++ 13-javascript-amazon-project/scripts/amazon.js
@@ -102,5 +102,7 @@ document.querySelectorAll('.js-add-to-cart')
         .innerHTML = cartQuantity;
 
       const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
+
+      addedMessage.classList.add('added-to-cart-visible');
     });
 });
