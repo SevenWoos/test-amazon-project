3edf5991f3cc78282bd23030378ddf4fb8d27f64
SevenWoos <swenwu96@gmail.com>
2026-06-23 22:07:57 -0700

    13e (main). Update the cart quantity with the selected cart quantity.

--- data/cart.js
+++ data/cart.js
@@ -37,15 +37,15 @@ export function addToCart(productId) {
   });
 
   const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
-  const quantity = quantitySelector.value;
+  const quantity = Number(quantitySelector.value);
 
   // If we find a matching duplicate item, just increase the quantity, instead of adding it again to the cart.
   if(matchingItem) {
-    matchingItem.quantity += 1;
+    matchingItem.quantity += quantity;
   } else {
     cart.push({
       productId: productId, 
-      quantity: 1
+      quantity: quantity
     });
   }
 
