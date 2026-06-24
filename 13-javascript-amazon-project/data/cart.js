b3d9549cfe16fce4b6149236f96c0d2f2cd9c9ed
SevenWoos <swenwu96@gmail.com>
2026-06-24 13:25:36 -0700

    Save cart in localStorage.

--- 13-javascript-amazon-project/data/cart.js
+++ 13-javascript-amazon-project/data/cart.js
@@ -1,4 +1,4 @@
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart')) || [{
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6', 
   quantity: 2
 }, 
@@ -7,6 +7,23 @@ export let cart = [{
   productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d', 
   quantity: 1
 }];
+
+// export let cart = JSON.parse(localStorage.getItem('cart'));
+// if (!cart) {
+//   cart = [{
+//   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6', 
+//   quantity: 2
+//   }, 
+
+//   {
+//     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d', 
+//     quantity: 1
+//   }]
+// }
+
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+};
 
 export function addToCart(productId) {
   // Check for duplicate items so we can increase quantity
@@ -30,6 +47,8 @@ export function addToCart(productId) {
       quantity: quantity
     });
   }
+
+  saveToStorage();
 };
 
 export function removeFromCart(productId) {
@@ -42,4 +61,6 @@ export function removeFromCart(productId) {
   });
 
   cart = newCart;
+
+  saveToStorage();
 };  
