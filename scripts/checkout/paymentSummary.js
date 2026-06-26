b0498c5207cc4cad61077419d6b8508d4b452a93
SevenWoos <swenwu96@gmail.com>
2026-06-26 16:13:54 -0700

    Save data and generate HTML for payment summary.

---
+++ scripts/checkout/paymentSummary.js
@@ -0,0 +1,78 @@
+import {cart} from '../../data/cart.js';
+import {getProduct} from '../../data/products.js';
+import {getDeliveryOption} from '../../data/deliveryOptions.js';
+import {formatCurrency} from '../utils/money.js';
+
+export function renderPaymentSummary() {
+  // Model(save the data)
+  let productPriceCents = 0;
+  let shippingPriceCents = 0;
+
+  cart.forEach((cartItem) => {
+    const product = getProduct(cartItem.productId);
+    productPriceCents += product.priceCents * cartItem.quantity;
+
+    // Calculate shipping costs.
+    const deliveryOption = getDeliveryOption(cartItem.deliveryOptionId);
+    shippingPriceCents += deliveryOption.priceCents;
+  });
+
+  // Calculate total before tax.
+  const totalBeforeTaxCents = productPriceCents + shippingPriceCents;
+
+  // Calculate the 10% tax.
+  const taxCents = totalBeforeTaxCents * 0.1;
+
+  // Calculate the toal
+  const totalCents = totalBeforeTaxCents + taxCents;
+
+
+  // View(generate the HTML)
+  const paymentSummaryHTML = `
+    <div class="payment-summary-title">
+      Order Summary
+    </div>
+
+    <div class="payment-summary-row">
+      <div>Items (3):</div>
+      <div class="payment-summary-money">
+        $${formatCurrency(productPriceCents)}
+      </div>
+    </div>
+
+    <div class="payment-summary-row">
+      <div>Shipping &amp; handling:</div>
+      <div class="payment-summary-money">
+        $${formatCurrency(shippingPriceCents)}
+      </div>
+    </div>
+
+    <div class="payment-summary-row subtotal-row">
+      <div>Total before tax:</div>
+      <div class="payment-summary-money">
+        $${formatCurrency(totalBeforeTaxCents)}
+      </div>
+    </div>
+
+    <div class="payment-summary-row">
+      <div>Estimated tax (10%):</div>
+      <div class="payment-summary-money">
+        $${formatCurrency(taxCents)}
+      </div>
+    </div>
+
+    <div class="payment-summary-row total-row">
+      <div>Order total:</div>
+      <div class="payment-summary-money">
+        $${formatCurrency(totalCents)}
+      </div>
+    </div>
+
+    <button class="place-order-button button-primary">
+      Place your order
+    </button>
+  `;
+
+  document.querySelector('.js-payment-summary')
+    .innerHTML = paymentSummaryHTML;
+}
