f6ba4967e2ae834057d71bb37e90a39c25735943
SevenWoos <swenwu96@gmail.com>
2026-06-26 16:18:58 -0700

    Make payment summary interactive with MVC.

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -4,12 +4,7 @@ import {formatCurrency} from '../utils/money.js';
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
-
-hello();
-
-const today = dayjs();
-const deliveryDate = today.add(7, 'days');
-console.log(deliveryDate.format('dddd, MMMM D'));
+import {renderPaymentSummary} from './paymentSummary.js';
 
 export function renderOrderSummary() {
   let cartSummaryHTML = ``;
@@ -138,6 +133,9 @@ export function renderOrderSummary() {
         );
         container.remove();
         updateCartQuantity();
+
+        // Regenerate the Payment Summary HTML when clicking delete
+        renderPaymentSummary();
       });
   });
 
@@ -197,6 +195,9 @@ export function renderOrderSummary() {
 
         // Regenerate HTML when choosing new delivery option using recursive call.
         renderOrderSummary();
+
+        // Regenerate the Payment Summary HTML when changing the delivery options.
+        renderPaymentSummary();
     });
   });
 };
